3a6e9b9f370289245922e97a245a430c192a085e
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2026-07-09 11:37:23 -0300

    Se realiza un nuevo componente para una tarea y poder reutilizarlo. Se muestran las tareas apropiadamente segun el store

--- src/pages/02-objects/JiraPage.tsx
+++ src/pages/02-objects/JiraPage.tsx
@@ -14,11 +14,6 @@ export const JiraPage = () => {
     useShallow((state) => state.getTaskByStatus('done')),
   );
 
-  console.log('All Tasks: ', allTasks);
-  console.log('Pending Tasks: ', pendigTasks);
-  console.log('In Progress Tasks: ', inProgressTasks);
-  console.log('Done Tasks: ', doneTasks);
-
   return (
     <>
       <h1>Tareas</h1>
@@ -26,11 +21,15 @@ export const JiraPage = () => {
       <hr />
 
       <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-        <JiraTasks title='Pendientes' value='pending' />
+        <JiraTasks title='Pendientes' value='open' tasks={pendigTasks} />
 
-        <JiraTasks title='Avanzando' value='in-progress' />
+        <JiraTasks
+          title='Avanzando'
+          value='in-progress'
+          tasks={inProgressTasks}
+        />
 
-        <JiraTasks title='Terminadas' value='done' />
+        <JiraTasks title='Terminadas' value='done' tasks={doneTasks} />
       </div>
     </>
   );
