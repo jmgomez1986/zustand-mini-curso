312ee306ba6421c08ac3a612b7aefd7b931a1325
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2026-07-15 15:39:09 -0300

    Se crea, se implementa y se consume el segundo slice 'GuestSlice' del WeddingStore

--- src/stores/wedding/index.ts
+++ src/stores/wedding/index.ts
@@ -1,15 +1,17 @@
 import { create } from 'zustand';
 import { createPersonSlice, PersonSlice } from './person.slice';
 import { devtools } from 'zustand/middleware';
+import { createGuestSlice, GuestSlice } from './guest.slice';
 
 //? Creaar el Store
-type ShareState = PersonSlice;
+type ShareState = PersonSlice & GuestSlice;
 
 // ...a: une el set, get, storeApi en un solo arreglo de parámetros
 export const useWeddingBoundStore = create<ShareState>()(
   devtools(
     (...a) => ({
       ...createPersonSlice(...a),
+      ...createGuestSlice(...a),
     }),
     { name: 'wedding-store' },
   ),
