8ec6913054657d830c62abe0a239779a6d8c3e3d
Matias Gomez <jmgametal@gmail.com>
2026-07-20 10:59:35 -0300

    Se implementa la navegacion y manejo de ruta protegida para el dashboard si esta autorizado o no mediante una nueva peticion para checkear el token

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,10 +1,12 @@
 import { FormEvent } from 'react';
 import { useAuthStore } from '../../stores';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
+	const navigate = useNavigate();
 	const loginUser = useAuthStore((state) => state.loginUser);
 
-	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+	const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		// const { username, password, remember } = event.target as HTMLFormElement;
 		const { username, password, remember } =
@@ -13,8 +15,13 @@ export const LoginPage = () => {
 				password: { value: string };
 				remember: { checked: boolean };
 			};
-		console.log(username.value, password.value, remember.checked);
-		loginUser(username.value, password.value);
+
+		try {
+			await loginUser(username.value, password.value);
+			navigate('/dashboard');
+		} catch (error) {
+			console.log('No se pudo autenticar');
+		}
 		// username.value = '';
 		// password.value = '';
 		// remember.checked = false;
--- src/stores/auth/auth.store.ts
+++ src/stores/auth/auth.store.ts
@@ -9,6 +9,7 @@ export interface AuthState {
 	user?: User;
 
 	loginUser: (email: string, password: string) => Promise<void>;
+	checkAuthStatus: () => Promise<void>;
 }
 
 const storeApi: StateCreator<AuthState> = (set) => ({
@@ -22,6 +23,17 @@ const storeApi: StateCreator<AuthState> = (set) => ({
 			set({ status: 'authorized', token, user });
 		} catch (error) {
 			set({ status: 'unauthorized', token: undefined, user: undefined });
+			throw 'Unauthorized';
+		}
+	},
+
+	checkAuthStatus: async () => {
+		try {
+			const { token, ...user } = await AuthService.checkStatus();
+			set({ status: 'authorized', token, user });
+		} catch (error) {
+			set({ status: 'unauthorized', token: undefined, user: undefined });
+			throw 'Unauthorized';
 		}
 	}
 
