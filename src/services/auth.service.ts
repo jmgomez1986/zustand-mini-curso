266b9e202a0c39cdd0bb9f12cd17d985224d0c36
Matias Gomez <jmgametal@gmail.com>
2026-07-20 08:40:08 -0300

    Se implementa metodo para consumir la validacion del tocken. Se agrega el interceptor para agregar a cada peticioon http el token grabado en el store

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,20 +1,38 @@
 import { AxiosError } from 'axios';
 import { tesloApi } from '../api/tesloApi';
-import { User } from '../interfaces';
+
+interface LoginResponse {
+	id: string;
+	email: string;
+	fullName: string;
+	isActive: boolean;
+	roles: string[];
+	token?: string;
+}
 
 export class AuthService {
-	static login = async (email: string, password: string): Promise<User> => {
+	static login = async (email: string, password: string): Promise<LoginResponse> => {
 		try {
-			const { data } = await tesloApi.post<User>('/auth/login', { email, password });
+			const { data } = await tesloApi.post<LoginResponse>('/auth/login', { email, password });
 			console.log(data);
 			return data;
 		} catch (error) {
 			if (error instanceof AxiosError) {
-				console.log(error.response?.data)
-				throw new Error(error.response?.data)
+				console.log(error.response?.data);
+				throw new Error(error.response?.data);
 			}
 			console.log(error);
 			throw new Error('Unable to login');
 		}
-	}
+	};
+
+	static checkStatus = async (): Promise<LoginResponse> => {
+		try {
+			const { data } = await tesloApi.get<LoginResponse>('/auth/check-status');
+			return data;
+		} catch (error) {
+			console.log(error);
+			throw new Error('Unauthorized');
+		}
+	};
 }
